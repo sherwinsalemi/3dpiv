4b45f7b85ab820092698ee93802049855a6ec38b
Sherwin Salemi <sherwin.salemi@gmail.com>
2024-01-01 21:07:32 -0800

    minor changes

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p bin
 if g++ src/*.cpp -I/usr/include/opencv4 -luvc -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -obin/3dpiv; then 
 echo "Build succeeded";
 sudo bin/3dpiv
