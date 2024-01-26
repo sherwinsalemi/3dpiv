5a9d6a7578848c9e6286103a0724bd971fcc868a
Sherwin Salemi <sherwin.salemi@gmail.com>
2024-01-26 10:16:49 -0800

    Linux compat build

--- build.sh
+++ build.sh
@@ -1,13 +1,24 @@
 mkdir -p bin
 
 # if [ $OSTYPE == "msys" ]; then
-if clang++ src/*.cc src/*.c -Iinc -Llib -lSDL2 -obin/3dpiv.exe -Wno-deprecated; then 
-	echo "Build succeeded";
-	exit 0;
-else 
-	echo "Build failed"; 
-	exit 1;
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	if clang++ src/*.cc src/*.c -Iinc -Llib -lSDL2 -obin/3dpiv -Wno-deprecated; then 
+		echo "Build succeeded";
+		exit 0;
+	else 
+		echo "Build failed"; 
+		exit 1;
+	fi
+else
+	if clang++ src/*.cc src/*.c -Iinc -Llib -lSDL2 -obin/3dpiv.exe -Wno-deprecated; then 
+		echo "Build succeeded";
+		exit 0;
+	else 
+		echo "Build failed"; 
+		exit 1;
+	fi
 fi
+
 
 
 
